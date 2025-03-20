da6cc82113bc8350563ed0bce4784d70a73a5a16
Ye Yint Aung Naing <yeyintaungnaing3535@gmail.com>
2025-03-21 06:47:03 +0800

    sorting prducts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,18 +11,73 @@ app.use(cors({
     allowedHeaders: ["Authorization", "Content-Type"],
 }))
 
-app.get("/products", async (req, res) => {
+app.get("/products", async (req , res) => {
     try{
+        let orderClause : string; 
+        const sortBy =  req.query.sortBy as string || ""
         const limit = parseInt(req.query.limit as string) || 16;
         const page = parseInt(req.query.page as string) || 1;
         const offset = (page - 1) * limit;
-        
+    
+
+        switch (sortBy) {
+            case "reviews_asc":
+              orderClause = `ORDER BY reviews ASC`;
+              break;
+            case "reviews_desc":
+              orderClause = `ORDER BY reviews DESC`;
+              break;
+            case "price_asc":
+              orderClause = `ORDER BY price ASC`;
+              break;
+            case "price_desc":
+              orderClause = `ORDER BY price DESC`;
+              break;
+            case "title_asc":
+              orderClause = `ORDER BY title ASC`;
+              break;
+            case "title_desc":
+              orderClause = `ORDER BY title DESC`;
+              break;
+            case "stars_asc":
+              orderClause = `ORDER BY stars ASC`;
+              break;
+            case "stars_desc":
+              orderClause = `ORDER BY stars DESC`;
+              break;
+            default:
+              orderClause = "ORDER BY reviews ASC"; 
+        }
+
         const result = await db.query(
-            'SELECT * FROM "120k_products" ORDER BY reviews DESC LIMIT $1 OFFSET $2',
+            `SELECT * FROM "120k_products" ${orderClause} LIMIT $1 OFFSET $2`,
             [limit, offset]
         );
         res.status(200).json(result.rows);
     }
+    catch(e) {
+        res.status(500).json({message : "Server error"})
+        console.log(e)
+    }
+})
+
+
+app.get("/products/search", async (req, res) => {
+    
+    try{
+        const searchQuery = req.query.searchQuery as string
+        console.log(searchQuery)
+        const limit = parseInt(req.query.limit as string) || 16;
+        const page = parseInt(req.query.page as string) || 1;
+        const offset = (page - 1) * limit;
+
+        const result = await db.query(
+            `SELECT * FROM "120k_products" WHERE "title" ILIKE $1 LIMIT $2 OFFSET $3`,
+            [`%${searchQuery}%`, limit, offset]  
+        );
+        res.json(result.rows);
+    }
+
     catch(e) {
         res.status(500).json({message : "Server error"})
         console.log(e)
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react"
 import { API_BASE_URL } from "../config";
 import "../../styles/Home.scss"
+import axios from "axios";
+
+
+// todo :  change the frontend url when sorting/filterig request '
+// still use states for queries , user clicks search - change url with that new query - make usesearchParam url dependancy for useeffect - 
+// useeffect will fetch data with the values in url instead
 
 interface Product{
     id : string;
@@ -14,10 +20,23 @@ interface Product{
     categoryname : string
 }
 
+const SORT_OPTIONS = [
+    { label: "Highest Reviews", value: "reviews_desc" },
+    { label: "Lowest Reviews", value: "reviews_asc" },
+    { label: "Highest Price", value: "price_desc" },
+    { label: "Lowest Price", value: "price_asc" },
+    { label: "A-Z Title", value: "title_asc" },
+    { label: "Z-A Title", value: "title_desc" },
+    { label: "Rating High to Low", value: "stars_desc" },
+    { label: "Rating Low to High", value: "stars_asc" },
+]
+
 export default function Home() {
 
     const [products, setProducts] = useState<Product[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(1)
+    const [sort, setSort] = useState<string>("reviews_desc")
+    const [searchQuery, setSearchQuery] = useState<string>("")
 
 
     useEffect(() => {
@@ -27,7 +46,7 @@ export default function Home() {
 
     async function fetchProducts() {
         try{
-            const response  = await axios.get(`${API_BASE_URL}/products?limit=16&page=${currentPage}`)
+            const response  = await axios.get(`${API_BASE_URL}/products?limit=16&page=${currentPage}&sortBy=${sort}`)
             setProducts(response.data as Product[])
 
         }
@@ -35,8 +54,23 @@ export default function Home() {
             console.log(e)
         }
     }
+    
+    async function searchProducts() {
+        if (searchQuery.length < 4) {
+            console.log('must be longer than 4 chracters')
+            return
+        }
+        try{
+            const response = await axios.get(`${API_BASE_URL}/products/search?limit=16&page=${currentPage}&searchQuery=${searchQuery}`);
+            setProducts(response.data as Product[])
+        }
+        catch(e) {
+            console.log(e)
+        }
+    }
 
     function incrementPage() {
+        //console.log('d')
         setCurrentPage((prev) => prev+1 )
     }
 
@@ -45,10 +79,26 @@ export default function Home() {
             setCurrentPage((prev) => prev-1)
         } 
     }
+    //console.log(sort)
 
     return (
         <div className="home">
-            <button onClick={fetchProducts}>fetch</button>
+            <div>
+                <button onClick={fetchProducts}>fetch</button>
+                <div className="dropdown-container">
+                    <select value={sort} onChange={(e) => setSort(e.target.value)}>
+                    {SORT_OPTIONS.map((option) => (
+                        <option key={option.value} value={option.value}>
+                        {option.label}
+                        </option>
+                    ))}
+                    </select>
+                </div>
+                <div>
+                    <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                    <button onClick={searchProducts}>search</button>
+                </div>
+            </div>
             <div className="products">
             {
                products && products.length > 0 && (
@@ -65,7 +115,7 @@ export default function Home() {
                )
             }
             </div>
-            <div>
+            <div className="pages">
                 <button onClick={decrementPage}>-</button>
                 <p>{currentPage}</p>
                 <button onClick={incrementPage}>+</button>
